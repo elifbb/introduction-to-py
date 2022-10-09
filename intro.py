4cf213d3d82b091d408a922a5bf166f2157ac206
elifbb <75088775+elifbb@users.noreply.github.com>
2022-10-10 00:33:18 +0300

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,322 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Seckin")
+
+
+# In[2]:
+
+
+print ("Hello world")
+
+
+# In[3]:
+
+
+message = "hello world"
+print(message)
+
+
+# In[4]:
+
+
+message = "ya allah bismillah"
+print(message)
+
+
+# In[5]:
+
+
+name ="ada lovelace"
+print(name.title ())
+
+
+# In[7]:
+
+
+name= "ada lovelace"
+print (name.upper())
+print (name.lower())
+
+
+# In[11]:
+
+
+first_name="ada"
+last_name="lovelace"
+full_name=first_name + " " +last_name
+print (full_name)
+
+
+# In[12]:
+
+
+print("hello, " + full_name.title() + "!")
+
+
+# In[13]:
+
+
+message= "hello, " + full_name.upper() + "!"
+print(message)
+
+
+# In[14]:
+
+
+print("Languages:\n\tPython\nC\tJava")
+
+
+# In[15]:
+
+
+fav_lang="python "
+
+
+# In[16]:
+
+
+fav_lang.rstrip()
+
+
+# In[17]:
+
+
+fav_lang.strip()
+
+
+# In[18]:
+
+
+print"hello world"
+
+
+# In[19]:
+
+
+3/2
+
+
+# In[22]:
+
+
+3**3
+
+
+# (3**3)*3
+
+# In[23]:
+
+
+(3**3)*3
+
+
+# In[24]:
+
+
+(3**3)**3
+
+
+# In[25]:
+
+
+0.2+0.1
+
+
+# In[26]:
+
+
+age = 23
+message = "Happy " + str(age) + "rd Birthday!"
+print(message)
+
+
+# In[28]:
+
+
+age="23"
+message= "Happy" +age+ "rd birthday"
+print(message)
+
+
+# In[29]:
+
+
+3/2
+
+
+# In[31]:
+
+
+#naber
+print("naber")
+
+
+# In[32]:
+
+
+The Zen of Python, by Tim Peters
+
+
+# In[33]:
+
+
+fruits=["apple", 'banana', "grapes"]
+print(fruits)
+
+
+# In[35]:
+
+
+print(fruits[0])
+
+
+# In[36]:
+
+
+print(fruits[1].title())
+
+
+# In[38]:
+
+
+print(fruits[-1])
+
+
+# In[39]:
+
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+
+# In[40]:
+
+
+motorcycles.append('ducati')
+print(motorcycles)
+
+
+# In[41]:
+
+
+motorcycles = []
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+print(motorcycles)
+
+
+# In[43]:
+
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles.insert(0, 'ducati')
+print(motorcycles)
+
+
+# In[44]:
+
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+del motorcycles[0]
+print(motorcycles)
+
+
+# In[46]:
+
+
+fruits=['apple', 'banana', 'grape']
+print(fruits)
+
+
+# In[47]:
+
+
+can=fruits.pop()
+print(fruits)
+print(can)
+
+
+# In[48]:
+
+
+fruits.pop()
+
+
+# In[49]:
+
+
+print(fruits)
+
+
+# In[50]:
+
+
+fruits=['apple', 'banana', 'grape']
+mak=fruitd.pop(1)
+print(mak)
+
+
+# In[52]:
+
+
+fruits=['apple', 'banana', 'grape']
+fruits.append('orange')
+print(fruits)
+
+
+# In[56]:
+
+
+print (fruits[0].upper())
+
+
+# In[58]:
+
+
+fruits=['apple', 'banana', 'grape']
+lasteaten=fruits.pop(1)
+print('I ate ' + lasteaten.title())
+
+
+# In[59]:
+
+
+fruits.sort()
+
+
+# In[60]:
+
+
+print(fruits)
+
+
+# In[61]:
+
+
+fruits.sort(reverse=True)
+print(fruits)
+
+
+# In[62]:
+
+
+fruits.sort(reverse=False)
+print(fruits)
+
+
+# In[ ]:
+
+
+(sorted(fruits))  # geçici olarak sortlar
+cars.reverse() # sırayı tersten yazar
+len(fruits) #kaç item
+[-1] #son itemi verir
+
